455a7c052113aff829c4bcaa68b900b08181d308
elias <elias.g.munene@gmail.com>
2024-03-21 05:16:54 +0300

    f.lab

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// returnFirstTwoDrivers function
+
 const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0, 2);
   }
   
-  // returnLastTwoDrivers function
+ 
   const returnLastTwoDrivers = function(drivers) {
     return drivers.slice(-2);
   }
   
-  // selectingDrivers array
+ 
   const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
   
-  // createFareMultiplier function
+  
   function createFareMultiplier(integer) {
     return function(fare) {
       return fare * integer;
     }
   }
   
-  // fareDoubler function
+ 
   const fareDoubler = createFareMultiplier(2);
   
   // fareTripler function
